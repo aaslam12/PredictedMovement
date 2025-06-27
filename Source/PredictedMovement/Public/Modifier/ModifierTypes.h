5090e1fd9f8b1b8d97023fb8dd94dd0de1547298
Jared Taylor <jaredtaylor.99@gmail.com>
2025-06-27 14:25:18 +1200

    Fix copyright notice

--- Source/PredictedMovement/Public/Modifier/ModifierTypes.h
+++ Source/PredictedMovement/Public/Modifier/ModifierTypes.h
@@ -1,12 +1,10 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Copyright (c) Jared Taylor
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "ModifierCompression.h"
 
-
-// DECLARE_DELEGATE_ThreeParams(FOnModifierChanged, FGameplayTag /*ModifierType*/, T /*ModifierLevel*/, T /*PrevLevel*/);
 
 template <typename T>
 struct TIsValidModifierEnum : std::false_type {};
